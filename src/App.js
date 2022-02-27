4a08f46d3e11794141be6109e81fca8541c33f47
Rosie <rosie.ross@workday.com>
2022-02-27 13:27:11 -0800

    feat: add instafeed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import InstaFeeds from './insta-feed/instafeed'
 
 import './App.scss';
 
@@ -11,6 +12,8 @@ export function App() {
       <h1 className="header">Rosalie Ross</h1>
 
     </Container>
+    <InstaFeeds token={"IGQVJVa0dLLVFnaVlHVkhrVFBWVWhqZA1hEWU5WZAmgwLUVxTDh2ZAVpWWlpFR2hEOWpJR2d1LVZADeGRFdndlYXQ0ME83b3ItQTY4Tk1FUkU2TVhHbklEa2FWS0FucnJ5SEw1UEhuWGV3"} limit={12}/>
+
   </div>
 );
   }
