97e7acc85cf4708f180aa7e62861d28938d88421
Rosie <rosie.ross@workday.com>
2022-02-27 16:53:10 -0800

    feat: add socials

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,12 +10,21 @@ export function Header() {
   return (
     <Navbar bg="light" expand="lg" fixed="top">
     <Container>
-    <Navbar.Brand href="#home">Rosalie Ross</Navbar.Brand>
+    <Navbar.Brand href="#home">Rosalie Ross </Navbar.Brand>
+    <a href='https://www.linkedin.com/in/rosalieross//'> 
+    <img src='src/assets/linkedin-icon.png' alt='instagram' style={{width: '35px'}}/>
+    </a>   
+ <a href='https://www.kickstarter.com/projects/1444231543/dont-let-ice-creams-be-dreams/description'>
+    <img src='src/assets/kickstarter-icon.png' alt='kickstarter'style={{width: '35px'}}/>
+    </a>
+    <a href='https://www.instagram.com/icecreamdreamsca/'> 
+    <img src='src/assets/instagram-icon.jpeg' alt='instagram' style={{width: '35px'}}/>
+    </a>
+
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
       <LinkContainer to="/">
-
         <Nav.Link >Home</Nav.Link>
         </LinkContainer>
         <LinkContainer to="/IceCream">
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 export function HomePage() {
   return (
+<div style={{display:'flex', flexDirection : 'column'}}>
+    <div style={{margin: '5px'}}>
 
+</div>
   <div style={{display:'flex', flexDirection:'row', margin: '15px' }}>
     <h2 style={{width: '50%', alignSelf: 'center'}}>Hi! I'm Rosie.</h2>
     <img src='src/assets/hiking.jpeg' alt='Snowshoeing in Utah!' width='50%'/>
-  </div>
+
+    </div> 
+    </div>
 );
   }
 
