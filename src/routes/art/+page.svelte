31e60d86fd4e039ad9b324b94fa312a6fa9c2a95
rosiewynner <32306013+rosiewynner@users.noreply.github.com>
2026-09-10 16:21:54 -0700

    optimize images

--- src/routes/art/+page.svelte
+++ src/routes/art/+page.svelte
@@ -1,9 +1,9 @@
 <div class="page">
 <div class="art-container">
-	<img alt="avocado" class="avocado" src="avocados.png" />
-	<img alt="moon" class="moon" src="moon.png" />
-	<img alt="fish" class="fish" src="fish.png" />
-	<img alt="painting" class="painting" src="painting2019.png" />
+	<img alt="avocado" class="avocado" src="avocados.jpg" />
+	<img alt="moon" class="moon" src="moon.avif" />
+	<img alt="fish" class="fish" src="fish.jpg" />
+	<img alt="painting" class="painting" src="painting2019.jpg" />
 </div>
 </div>
 
