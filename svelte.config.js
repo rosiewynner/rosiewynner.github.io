63dcf614bb60b081b4bbb76b7b988cee80b61196
rosiewynner <32306013+rosiewynner@users.noreply.github.com>
2026-09-09 18:06:33 -0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: '404.html', // Allows client-side routing to function normally
+			fallback: '404.html', 
 			precompress: false,
 			strict: true
 		}),
