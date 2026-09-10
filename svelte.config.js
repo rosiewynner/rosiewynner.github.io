368d828dc65c136e550d6a134c1d27f3ab6b84e6
rosiewynner <32306013+rosiewynner@users.noreply.github.com>
2026-09-09 17:12:51 -0700

    add a github action

--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,13 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: '404.html', // Allows client-side routing to function normally
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : '/rosiewynner'
+		}
 	}
 };
 
